df71ace2ffe1acb9976421fd44d4ddd68eb1c4ea
Nalin Sakthivel <nalin.s@technotackle.com>
2023-08-07 18:52:56 +0530

    initial commit with login page

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Style.css";
+
+function Login() {
+  return (
+    <>
+      <head>
+        <title>hello</title>
+      </head>
+      <div className="mainContainer">
+        <h1 className="heading"> Login</h1>
+        <div className="inputContainer">
+          <h4 className="inputLabel">User Name</h4>
+          <input className="input" type="text" />
+        </div>
+        <div className="inputContainer">
+          <h4 className="inputLabel">Password</h4>
+          <input className="input" type="password" maxLength={8} />
+        </div>
+        <div className="buttonContainer">
+          <button
+            className="button"
+            type="submit"
+            onClick={() => {
+              console.log("hello :>> ");
+            }}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
